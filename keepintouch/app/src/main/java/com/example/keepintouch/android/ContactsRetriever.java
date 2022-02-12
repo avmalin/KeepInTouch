c882358dfbac57b2e8da8ef6fc1a7cc52fb24a5e
Avihai Malin <avihaimalin@gmail.com>
2022-02-12 23:45:01 +0200

    added android ContactsRetriever

---
+++ keepintouch/app/src/main/java/com/example/keepintouch/android/ContactsRetriever.java
@@ -0,0 +1,9 @@
+package com.example.keepintouch.android;
+
+import com.example.keepintouch.types.Contact;
+
+import java.util.List;
+
+public interface ContactsRetriever {
+    List<Contact> getContacts();
+}
