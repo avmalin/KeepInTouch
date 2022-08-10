05845474a9e4731d1ab0714417da7aa5954d5933
Avihai Malin <avihaimalin@gmail.com>
2022-08-10 15:24:06 +0300

    add contacts_list_item.xml add contacts_list_view.xml

--- keepintouch/app/src/main/java/com/example/keepintouch/MainActivity.java
+++ keepintouch/app/src/main/java/com/example/keepintouch/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.keepintouch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * Defines an array that contains column names to move from
+     * the Cursor to the ListView.
+     */
+    private final static String[] FROM_COLUMNS = {
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+    };
+    ListView contactsList;
+    private SimpleCursorAdapter cursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
